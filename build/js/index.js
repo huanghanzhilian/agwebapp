62018c0f5b7cdc796ce8b7347951082bf02c91ae
Jipeng Huang <1319639755@qq.com>
2017-08-27 02:26:59 +0800

    updata

--- build/js/index.js
+++ build/js/index.js
@@ -37,6 +37,27 @@ angular.module('app').config(['$stateProvider', '$urlRouterProvider',function($s
 	$urlRouterProvider.otherwise('main');
 }]);
 'use strict';
+angular.module('app').controller('mainCtrl', ['$http', '$scope', function($http, $scope){
+	$scope.list=[{
+		id:'1',
+		name:'销售',
+		imgSrc:'image/item_image3.jpg',
+		conpanyName:'前度',
+		city:'北京',
+		industry:'互联网',
+		time:'2017-06-06'
+	},{
+		id:'2',
+		name:'前端',
+		imgSrc:'image/item_image2.png',
+		conpanyName:'拉钩',
+		city:'北京',
+		industry:'互联网',
+		time:'2017-06-06'
+	}];
+}]);
+
+'use strict';
 angular.module('app').directive('appFoot', [function(){
   return {
     restrict: 'A',
@@ -75,25 +96,4 @@ angular.module('app').directive('appPositionList', [function(){
     	data:'='
     },
   };
-}]);
-
-'use strict';
-angular.module('app').controller('mainCtrl', ['$http', '$scope', function($http, $scope){
-	$scope.list=[{
-		id:'1',
-		name:'销售',
-		imgSrc:'image/item_image3.jpg',
-		conpanyName:'前度',
-		city:'北京',
-		industry:'互联网',
-		time:'2017-06-06'
-	},{
-		id:'2',
-		name:'前端',
-		imgSrc:'image/item_image2.png',
-		conpanyName:'拉钩',
-		city:'北京',
-		industry:'互联网',
-		time:'2017-06-06'
-	}];
 }]);
